a3309301a6c764e5c24705c7721f9ffbf8451966
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-03-08 15:16:08 -0300

    Adiciona botoes de explorar e sua funcionalidade de redirecionamento

--- src/Pages/Explore/Explore.jsx
+++ src/Pages/Explore/Explore.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Header } from '../../components';
 
 function Explore() {
   return (
     <>
       <Header name="Explore" hasIcons={ false } />
-      <div>Recipes</div>
+      <div>
+        <Link to="/explore/foods">
+          <button type="button" data-testid="explore-foods">Explore Foods</button>
+        </Link>
+        <Link to="/explore/drinks">
+          <button type="button" data-testid="explore-drinks">Explore Drinks</button>
+        </Link>
+      </div>
     </>
   );
 }
