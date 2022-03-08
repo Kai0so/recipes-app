722a849c7d613c9dccfa09d1a7f5347eecc5d618
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2022-03-08 15:42:39 -0300

    Adiciona botoes de pesquisa por ingrediente e nacionalidade e sua funcionalidade de redirecionamento

--- src/Pages/ExploreDrinks/ExploreDrinks.jsx
+++ src/Pages/ExploreDrinks/ExploreDrinks.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Header } from '../../components';
 
 function ExploreDrinks() {
   return (
     <>
       <Header name="Explore Drinks" hasIcons={ false } />
-      <div>Recipes</div>
+      <div>
+        <Link to="/explore/drinks/ingredients">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            By Ingredient
+          </button>
+        </Link>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Surprise me!
+        </button>
+      </div>
     </>
   );
 }
--- src/Pages/ExploreFoods/ExploreFoods.jsx
+++ src/Pages/ExploreFoods/ExploreFoods.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Header } from '../../components';
 
 function ExploreFoods() {
   return (
     <>
       <Header name="Explore Foods" hasIcons={ false } />
-      <div>Recipes</div>
+      <div>
+        <Link to="/explore/foods/ingredients">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+          >
+            By Ingredient
+          </button>
+        </Link>
+        <Link to="/explore/foods/nationalities">
+          <button
+            type="button"
+            data-testid="explore-by-nationality"
+          >
+            By Nationality
+          </button>
+        </Link>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Surprise me!
+        </button>
+      </div>
     </>
   );
 }
