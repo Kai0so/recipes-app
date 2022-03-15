3284f4ac1d22f545e9de1abe40d767e5f245efef
Yamamoto Andre <andre.yamamoto07@gmail.com>
2022-03-15 18:33:05 -0300

    req 60'

--- src/Pages/Favorite/Favorite.jsx
+++ src/Pages/Favorite/Favorite.jsx
@@ -1,14 +1,69 @@
-import React, { useState } from 'react';
-import { ButtonFavorites, Header } from '../../components';
+import React, { useEffect, useState } from 'react';
+import { Header } from '../../components';
 
 function Favorite() {
-  const [saveRecipes, setsaveRecipes] = useState([]);
+  const [foodList, setFoodList] = useState([]);
+
+  useEffect(() => {
+    setFoodList(JSON.parse(localStorage.getItem('favoriteRecipes')));
+  }, []);
+
+  console.log(foodList);
+  // const [saveRecipes, setsaveRecipes] = useState([]);
   return (
     <section>
       <Header name="Favorite Recipes" hasSearchIcon={ false } hasProfileIcon />
+      <nav>
+        <button data-testid="filter-by-all-btn" type="button">All</button>
+        <button data-testid="filter-by-food-btn" type="button">Food</button>
+        <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+      </nav>
 
-      <ButtonFavorites />
+      <div>
+        {foodList.length > 0
+          ? (foodList.map((iten, index) => (
+            <div key={ iten.id }>
+              <section
+                id={ iten.id }
+              >
+                <h1
+                  data-testid={ `${index}-horizontal-name` }
+                >
+                  {iten.name}
 
+                </h1>
+                <img
+                  alt={ iten.name }
+                  data-testid={ `${index}-horizontal-image` }
+                  src={ iten.image }
+                />
+
+                <h2
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {iten.category}
+
+                </h2>
+
+              </section>
+              <section>
+                <button
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  type="button"
+                >
+                  share
+                </button>
+                <button
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                  type="button"
+                >
+                  favorito
+                </button>
+              </section>
+            </div>
+          )))
+          : null }
+      </div>
     </section>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,5 @@ export { default as Header } from './Header/Header';
 export { default as SearchBar } from './SearchBar/SearchBar';
 export { default as Footer } from './Footer/Footer';
 export { default as CategButtons } from './CategButtons/CategButtons';
-export { default as ButtonFavorites } from './ButtonFavorites/ButtonFavorites';
-export { default as NaviFilterButtons } from './FilterButton/FilterButtons';
 export { default as Food } from './Food/Food';
 export { default as Drink } from './Drink/Drink';
